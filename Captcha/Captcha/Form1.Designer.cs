6fc99e757db77e56f09bc5a794eaf149d3c3395a
Jesus Antonio JD <juarez_devora_ja@hotmail.com>
2017-03-23 08:08:09 -0800

    Add files via upload

---
+++ Captcha/Captcha/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Captcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.recaptchabtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 56);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.MouseLeave += new System.EventHandler(this.textBox1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Escribe el captcha de arriba";
+            // 
+            // recaptchabtn
+            // 
+            this.recaptchabtn.Image = ((System.Drawing.Image)(resources.GetObject("recaptchabtn.Image")));
+            this.recaptchabtn.Location = new System.Drawing.Point(224, 74);
+            this.recaptchabtn.Name = "recaptchabtn";
+            this.recaptchabtn.Size = new System.Drawing.Size(70, 79);
+            this.recaptchabtn.TabIndex = 3;
+            this.recaptchabtn.UseVisualStyleBackColor = true;
+            this.recaptchabtn.Click += new System.EventHandler(this.recaptchabtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(329, 161);
+            this.Controls.Add(this.recaptchabtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CAPTCHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button recaptchabtn;
+    }
+}
+
---
+++ Captcha/Captcha/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captcha
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        //Declaramos un arreglo en el cual tenga las imagenes
+        string[] imagenes = new string[5];
+        string[] nombres = new string[5];
+        int posicion = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            //Para guardar la direccion de la imagen usamos @
+            imagenes[0] = @"AllYues arcades.png";
+            nombres[0] = "AllYues arcades";
+            imagenes[1] = @"kfnh hqck.jpg";
+            nombres[1] = "kfnh hqck";
+            imagenes[2] = @"Levelers critic.jpg";
+            nombres[2] = "Levelers critics";
+            imagenes[3] = @"morning ovelooks.jpg";
+            nombres[3] = "morning overlooks";
+            imagenes[4] = @"nitz haiphong.jpg";
+            nombres[4] = "nitz haiphong";
+            Random r = new Random();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            iniciar();
+        }
+        private void iniciar()
+        {
+            this.BackColor = Color.FromArgb(224, 224, 224);
+            int posicion2 = r.Next(0, 5);
+            pictureBox1.ImageLocation = imagenes[posicion2];
+
+        }
+
+        private void recaptchabtn_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(224, 224, 224);
+            posicion = r.Next(0, 5);
+            //Imprime la imagen en la posicion 0
+            pictureBox1.ImageLocation = imagenes[posicion];
+        }
+        private void final()
+        {
+            
+            this.BackColor = Color.FromArgb(224, 224, 224);
+            posicion = r.Next(0, 5);
+            //Imprime la imagen en la posicion 0
+            pictureBox1.ImageLocation = imagenes[posicion];
+        }
+
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"Goods.wav");
+            simpleSound.Play();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            string aux1 = "";
+            
+            //string NombreArchivo = System.IO.Path.GetFileName(pictureBox1);
+            aux1 = textBox1.Text;
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (aux1 == nombres[0] && posicion == 0)
+                {
+                    this.BackColor = Color.Green;
+
+
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = "Goods.wav";
+                    player.Play();
+                    textBox1.Text = "";
+                    DialogResult result =MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if(result== DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+
+                else if (aux1 == nombres[1] && posicion == 1)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result1 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result1 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[2] && posicion == 2)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result2 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result2 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[3] && posicion == 3)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result3 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result3 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[4] && posicion == 4)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result4 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+                    if (result4 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                    textBox1.Text = "";
+                    DialogResult result5 = MessageBox.Show("You are a machine!!", "MACHINE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (result5 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+            }
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            string aux1 = "";
+            //string NombreArchivo = System.IO.Path.GetFileName(pictureBox1);
+            aux1 = textBox1.Text;
+                if (aux1 == nombres[0] && posicion == 0)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+
+                else if (aux1 == nombres[1] && posicion == 1)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result1 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result1 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[2] && posicion == 2)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result2 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result2 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[3] && posicion == 3)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result3 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result3 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else if (aux1 == nombres[4] && posicion == 4)
+                {
+                    this.BackColor = Color.Green;
+                    textBox1.Text = "";
+                    DialogResult result4 = MessageBox.Show("Felicidades es correcto!!", "Human", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    if (result4 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                    textBox1.Text = "";
+                    DialogResult result5 = MessageBox.Show("You are a machine!!", "MACHINE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (result5 == DialogResult.OK)
+                    {
+                        final();
+                    }
+                }
+            }
+    }
+}
